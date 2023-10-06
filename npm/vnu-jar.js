6e2acbaed145ef0e4eb03fa793708ae26f753f7b
Anrudh Kumar <121420936+Anrudh12@users.noreply.github.com>
2023-10-06 22:17:45 +0530

    Update vnu-jar.js

--- npm/vnu-jar.js
+++ npm/vnu-jar.js
@@ -15,8 +15,7 @@ childProcess.exec('java -version', (error, stdout, stderr) => {
 
   const is32bitJava = !/64-Bit/.test(stderr)
 
-  // vnu-jar accepts multiple ignores joined with a `|`.
-  // Also note that the ignores are regular expressions.
+
   const ignores = [
     // "autocomplete" is included in <button> and checkboxes and radio <input>s due to
     // Firefox's non-standard autocomplete behavior - see https://bugzilla.mozilla.org/show_bug.cgi?id=654072
