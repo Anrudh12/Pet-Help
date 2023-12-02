c048ce72776e374a2c1660e7e208989658bc9c89
Anrudh Kumar <121420936+Anrudh12@users.noreply.github.com>
2023-12-02 16:29:40 +0530

    Update pet_shop_db.sql

--- database/pet_shop_db.sql
+++ database/pet_shop_db.sql
@@ -3,8 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 22, 2021 at 10:17 AM
--- Server version: 10.4.19-MariaDB
+
 -- PHP Version: 8.0.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
